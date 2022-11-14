ce93298a9bac9eac5de6872d84dd851b24c28d8c
marcojimenezurena <116891701+marcojimenezurena@users.noreply.github.com>
2022-11-14 13:25:21 +0100

    change static class to singleton

--- src/main/java/com/curso/baptisteriopaleocristianodelsigloprimero/TicketMachine.java
+++ src/main/java/com/curso/baptisteriopaleocristianodelsigloprimero/TicketMachine.java
@@ -14,17 +14,36 @@ import java.util.HashMap;
  * @author mjimen19
  */
 public class TicketMachine {
-    private static HashMap<LocalDate,Integer> soldTickets = new HashMap<>();
-    private static final int maxTicketPerDay = 10;
-    private static final int ticketPrice = 4;
+    private static TicketMachine instance;
+    private HashMap<LocalDate,Integer> soldTickets = new HashMap<>();
+    private final int maxTicketPerDay = 10;
+    private final int ticketPrice = 4;
 
-    private static boolean existTicket(LocalDate date){
+    private TicketMachine(){
+        
+    }
+    
+    public static TicketMachine getInstance(){
+        if (instance == null) {
+            instance = new TicketMachine();
+        }
+        return instance;
+    }
+    
+    /*
+        Returns true if there are at least one ticket sold for the date specified
+    */
+    private boolean existTicket(LocalDate date){
         return soldTickets.containsKey(date);
     }
 
     
-    private static boolean hasEnoughTickets(LocalDate date, int numTickets){
-        boolean hasEnough = false;
+    /*
+        returns true if the number of tickets you want to buy are less
+        or equal the amount of tickets sold in the specified date
+    */
+    private boolean hasEnoughTickets(LocalDate date, int numTickets){
+        boolean hasEnough;
         if(existTicket(date)){
             hasEnough = numTickets <= maxTicketPerDay - soldTickets.get(date);
         }else{
@@ -33,16 +52,25 @@ public class TicketMachine {
         return hasEnough;
     }
     
-    private static boolean hasEnoughMoney(int money, int numTickets){
+    /*
+        returns true if the money given is exactly equal to the price of the tickets specified
+    */
+    private boolean hasEnoughMoney(int money, int numTickets){
         return money == numTickets * ticketPrice;
     }
     
-    public static void buy(LocalDate date, int money, int numTickets){
+    /*
+        given a specified date, amount of money and number of tickets, checks if it can buy
+        and in that case, adds tickets sold to the hashmap
+    */
+    public void buy(LocalDate date, int money, int numTickets) throws IllegalArgumentException{
         
         if(!hasEnoughMoney(money,numTickets)){
-            System.out.println("EL dineros me lo das enteroooo, no doi cambiooo morosoooo");
+            throw new IllegalArgumentException("El dineros me lo das enteroooo, no doi cambiooo morosoooo");
+//            System.out.println("EL dineros me lo das enteroooo, no doi cambiooo morosoooo");
         }else if (!hasEnoughTickets(date, numTickets)){
-            System.out.println("Eso son muchoos tikees para tan poco baptisterioooo");
+            throw new IllegalArgumentException("Eso son muchoos tikees para tan poco baptisterioooo");
+//            System.out.println("Eso son muchoos tikees para tan poco baptisterioooo");
         }else{
             if(!existTicket(date)){
                 soldTickets.put(date, numTickets);
@@ -54,7 +82,10 @@ public class TicketMachine {
         }
     }
     
-    public static int getAvailableTickets(LocalDate date){
+    /*
+        return the number of tickets left to sell at a specified date
+    */
+    public int getAvailableTickets(LocalDate date){
         int numTickets;
         if(!existTicket(date)){
             numTickets = maxTicketPerDay;
@@ -64,7 +95,10 @@ public class TicketMachine {
         return numTickets;
     }
     
-    public static String getAllTicketsSold(){
+    /*
+        return a text with the info of all tickets sold each days
+    */
+    public String getAllTicketsSold(){
         String text = "";
         
         for(LocalDate date : soldTickets.keySet()){
